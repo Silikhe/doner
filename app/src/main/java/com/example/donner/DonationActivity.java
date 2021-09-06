2c9f0914a60baf7867a07c3f5026959a1c2396a4
silikhe <silaswan@gmail.com>
2021-09-07 02:25:31 +0300

    feature(donations):add input viewer

--- app/src/main/java/com/example/donner/DonationActivity.java
+++ app/src/main/java/com/example/donner/DonationActivity.java
@@ -16,33 +16,33 @@ public class DonationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation);
 
-        ViewPager viewPager = findViewById(R.id.viewPager);
-
-        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
-        pagerAdapter.addFragmet(new LoginFragment());
-//        pagerAdapter.addFragmet(new RegisterFragment());
-        viewPager.setAdapter(pagerAdapter);
+//        ViewPager viewPager = findViewById(R.id.viewPager);
+//
+//        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+//        pagerAdapter.addFragmet(new LoginFragment());
+////        pagerAdapter.addFragmet(new RegisterFragment());
+//        viewPager.setAdapter(pagerAdapter);
     }
 
-    static class AuthenticationPagerAdapter extends FragmentPagerAdapter {
-        private ArrayList<Fragment> fragmentList = new ArrayList<>();
-
-        public AuthenticationPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int i) {
-            return fragmentList.get(i);
-        }
-
-        @Override
-        public int getCount() {
-            return fragmentList.size();
-        }
-
-        void addFragmet(Fragment fragment) {
-            fragmentList.add(fragment);
-        }
-    }
+//    static class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+//        private ArrayList<Fragment> fragmentList = new ArrayList<>();
+//
+//        public AuthenticationPagerAdapter(FragmentManager fm) {
+//            super(fm);
+//        }
+//
+//        @Override
+//        public Fragment getItem(int i) {
+//            return fragmentList.get(i);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return fragmentList.size();
+//        }
+//
+//        void addFragmet(Fragment fragment) {
+//            fragmentList.add(fragment);
+//        }
+//    }
 }
--- app/src/main/java/com/example/donner/LoginFragment.java
+++ app/src/main/java/com/example/donner/LoginFragment.java
@@ -2,13 +2,24 @@ package com.example.donner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class LoginFragment extends Fragment {
 
-public class LoginFragment extends AppCompatActivity {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_login, container, false);
     }
+
 }
