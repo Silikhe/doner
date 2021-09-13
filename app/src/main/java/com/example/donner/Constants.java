171c2b538954e6a1ba0b40bd4f70e85aa4453f88
silikhe <silaswan@gmail.com>
2021-09-13 15:56:40 +0300

    Adding required dependancies

---
+++ app/src/main/java/com/example/donner/Constants.java
@@ -0,0 +1,4 @@
+package com.example.donner;
+
+public class Constants {
+}
--- app/src/main/java/com/example/donner/DonationActivity.java
+++ app/src/main/java/com/example/donner/DonationActivity.java
@@ -3,6 +3,8 @@ package com.example.donner;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,22 +15,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DonationActivity extends AppCompatActivity {
-    private ListView mListView;
-    private String[] donations = new String[]{
-            "Mukuru Donations", "Kijabe Donations", "Nairobi donations", "Silas Donations",
-            "Tevin Donations", "Mike Donations",
-            "Yvonne Donations"
-    };
+//    private ListView mListView;
+//    private String[] donations = new String[]{
+//            "Mukuru Donations", "Kijabe Donations", "Nairobi donations", "Silas Donations",
+//            "Tevin Donations", "Mike Donations",
+//            "Yvonne Donations"
+//    };
 
+    RecyclerView recyclerView;
+    ArrayList<Blogs> blogsArrayList;
+    RecyclerViewAdapter recyclerViewAdapter;
+    String title;
+    String displayName;
+    String img_url;
+    String content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation);
 
-        mListView = (ListView) findViewById(R.id.listview);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, donations);
-        mListView.setAdapter(adapter);
+        recyclerView = findViewById(R.id.blogs_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        blogsArrayList = new ArrayList<>();
+
+        recyclerViewAdapter = new RecyclerViewAdapter(this, blogsArrayList);
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+
+
+//        mListView = (ListView) findViewById(R.id.listview);
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, donations);
+//        mListView.setAdapter(adapter);
 
     }
 }
