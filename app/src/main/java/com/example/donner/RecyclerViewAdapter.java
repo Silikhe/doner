224417ecab04b887da8f98bcc0279c58687d796d
silikhe <silaswan@gmail.com>
2021-09-14 09:24:50 +0300

    changing contents from html formart

--- app/src/main/java/com/example/donner/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/donner/RecyclerViewAdapter.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.jsoup.Jsoup;
+import org.w3c.dom.Document;
+
 import java.util.ArrayList;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
@@ -33,12 +36,24 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+
+        //get data
         Blogs blogs = blogsArrayList.get(position);
+
+
         holder.blogContent.setText(blogs.content);
         holder.blogTitle.setText(blogs.title);
         holder.blogName.setText(blogs.displayName);
         holder.blogImg.setImageURI(Uri.parse(blogs.img_url));
 
+        String authorName = blogs.getDisplayName();
+        String contents = blogs.getContent();
+        String title = blogs.getTitle();
+        String url = blogs.getImg_url();
+
+
+//change content descr from html formart
+        Document document = Jsoup.parse(contents)
     }
 
     @Override
