65d6e6f2006450d2e316b0843283c98213951deb
silikhe <silaswan@gmail.com>
2021-09-27 00:13:03 +0300

    Added custom map

--- app/src/main/java/com/example/donner/LocationActivity.java
+++ app/src/main/java/com/example/donner/LocationActivity.java
@@ -2,6 +2,7 @@ package com.example.donner;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.LocationRequest;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,10 +13,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.donner.databinding.ActivityLocationBinding;
 
+import java.util.Vector;
+
 public class LocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityLocationBinding binding;
+    MarkerOptions marker;
+    LatLng ctrLocation;
+    Vector<MarkerOptions> markerOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,29 @@ public class LocationActivity extends FragmentActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        ctrLocation = new LatLng(-1.286389, 36.817223);
+
+        markerOptions = new Vector<>();
+        markerOptions.add(new MarkerOptions().title("parklands Hockey field")
+                .position(new LatLng(1.2606459323111758 , 36.82733593696287))
+                .snippet("Parkland Recross Donation Centre"));
+
+        markerOptions.add(new MarkerOptions().title("Karen Langata")
+                .position(new LatLng(1.3219694060948566, 36.70695216044942))
+                .snippet("Donation Centre Langata"));
+
+        markerOptions.add(new MarkerOptions().title("Opposite Machakos GK Prison")
+                .position(new LatLng(1.5169552659092207, 37.261526584625244))
+                .snippet("Machakos Donation Centre"));
+
+
+//        marker = new MarkerOptions().title("Karen Langata")
+//                .position(new LatLng(1.3219694060948566, 36.70695216044942))
+//                .snippet("Donation Centre Langata");
     }
+
+
 
     /**
      * Manipulates the map once available.
@@ -44,8 +72,14 @@ public class LocationActivity extends FragmentActivity implements OnMapReadyCall
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ctrLocation, 9));
+
+        for (MarkerOptions mark : markerOptions) {
+            mMap.addMarker(mark);
+        }
     }
+
+
 }
